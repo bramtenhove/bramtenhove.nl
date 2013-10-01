b784323f831e77560b987b65eebd1845a87cde1e
Bram ten Hove <bram.ten.hove@goalgorilla.com>
2013-10-01 14:38:27 +0200

    Making the site responsive

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
+(function($) {
+  $(function() {
+    pull = $('#pull-menu');
+    menu = $('#main-menu ul.menu');
 
+    $(pull).on('click', function(e) {
+      e.preventDefault();
+      menu.stop().slideToggle(150);
+    });
+  });
+
+  // Back to normal.
+  $(window).resize(function(){
+    if(pull.is(':hidden')) {
+      menu.removeAttr('style');
+    }
+  });
+
+})(jQuery);
