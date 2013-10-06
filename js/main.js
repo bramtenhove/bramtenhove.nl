d8a6287aed46f8b73c6c0fdc9ed1345e4bb04ed7
Bram ten Hove <bram.ten.hove@goalgorilla.com>
2013-10-06 15:08:11 +0200

    Added a responsive slider

--- js/main.js
+++ js/main.js
@@ -1,19 +1,36 @@
-(function($) {
-  $(function() {
-    pull = $('#pull-menu');
-    menu = $('#main-menu ul.menu');
+var app = {
 
-    $(pull).on('click', function(e) {
-      e.preventDefault();
-      menu.stop().slideToggle(150);
+  onReady: function() {
+    $(function() {
+      pull = $('#pull-menu');
+      menu = $('#main-menu ul.menu');
+
+      $(pull).on('click', function(e) {
+        e.preventDefault();
+        menu.stop().slideToggle(150);
+      });
     });
-  });
 
-  // Back to normal.
-  $(window).resize(function(){
-    if(pull.is(':hidden')) {
-      menu.removeAttr('style');
-    }
-  });
+    // Back to normal.
+    $(window).resize(function(){
+      if (pull.is(':hidden')) {
+        menu.removeAttr('style');
+      }
+    });
 
-})(jQuery);
+    // Start the image slider.
+    $(".image-slides").responsiveSlides({
+      auto: true,
+      speed: 500,
+      timeout: 5000,
+      pager: false,
+      nav: false,
+      pause: true
+    });
+  }
+
+};
+
+$(document).ready(
+  app.onReady
+);
